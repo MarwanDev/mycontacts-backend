ac20cb98bc1fe9999291737ac1dc4bf6278fd2f7
Marwan <marwaneg@aucegypt.edu>
2023-09-05 19:24:27 +0300

    Create routes

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -1,16 +1,23 @@
 const express = require("express");
 const router = express.Router();
-const {
-  getContacts,
-  createContact,
-  getContact,
-  updateContact,
-  deleteContact,
-} = require("../controllers/contactController");
-// const validateToken = require("../middleware/validateTokenHandler");
+// const {
+//   getContacts,
+//   createContact,
+//   getContact,
+//   updateContact,
+//   deleteContact,
+// } = require("../controllers/contactController");
+// // const validateToken = require("../middleware/validateTokenHandler");
 
-// router.use(validateToken);
-router.route("/").get(getContacts).post(createContact);
-router.route("/:id").get(getContact).put(updateContact).delete(deleteContact);
+// // router.use(validateToken);
+// router.route("/").get(getContacts).post(createContact);
+// router.route("/:id").get(getContact).put(updateContact).delete(deleteContact);
+
+// module.exports = router;
+
+
+router.route("/").get((req, res) => {
+    res.status(200).json({message: "Get all messages"})
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
-const errorHandler = require("./middleware/errorHandler");
 const connectDb = require("./config/dbConnection");
+// const errorHandler = require("./middleware/errorHandler");
 const dotenv = require("dotenv").config();
 
 connectDb();
@@ -10,7 +10,11 @@ const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use("/api/contacts", require("./routes/contactRoutes"));
-app.use(errorHandler);
+// app.use("/api/contacts", (req, res) => {
+//   res.send("Get all contacts");
+// });
+// app.use("/api/users", require("./routes/userRoutes"));
+// app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
