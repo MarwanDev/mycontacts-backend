fbb228f5557a6f8e5d5a507ee0df6d3304c1e419
Marwan <marwaneg@aucegypt.edu>
2023-09-07 19:58:34 +0300

    Create contact model

--- models/contactModel.js
+++ models/contactModel.js
@@ -2,11 +2,6 @@ const mongoose = require("mongoose");
 
 const contactSchema = mongoose.Schema(
   {
-    user_id: {
-      type: mongoose.Schema.Types.ObjectId,
-      required: true,
-      ref: "User",
-    },
     name: {
       type: String,
       required: [true, "Please add the contact name"],
--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -1,19 +1,19 @@
 const express = require("express");
 const router = express.Router();
-// const {
-//   getContacts,
-//   createContact,
-//   getContact,
-//   updateContact,
-//   deleteContact,
-// } = require("../controllers/contactController");
-// // const validateToken = require("../middleware/validateTokenHandler");
+const {
+  getContacts,
+  createContact,
+  getContact,
+  updateContact,
+  deleteContact,
+} = require("../controllers/contactController");
+// const validateToken = require("../middleware/validateTokenHandler");
 
-// // router.use(validateToken);
-// router.route("/").get(getContacts).post(createContact);
-// router.route("/:id").get(getContact).put(updateContact).delete(deleteContact);
+// router.use(validateToken);
+router.route("/").get(getContacts).post(createContact);
+router.route("/:id").get(getContact).put(updateContact).delete(deleteContact);
 
-// module.exports = router;
+module.exports = router;
 
 
 router.route("/").get((req, res) => {
