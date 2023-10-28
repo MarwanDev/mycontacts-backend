9137ab16b0087159c8b2fa1850aeec3cfa57353e
Marwan <marwaneg@aucegypt.edu>
2023-10-29 00:47:46 +0200

    Create current user GET route

--- controllers/userController.js
+++ controllers/userController.js
@@ -47,27 +47,27 @@ const registerUser = asyncHandler(async (req, res) => {
 const loginUser = asyncHandler(async (req, res) => {
     const { email, password } = req.body;
     if (!email || !password) {
-      res.status(400);
-      throw new Error("All fields are mandatory!");
+        res.status(400);
+        throw new Error("All fields are mandatory!");
     }
     const user = await User.findOne({ email });
     //compare password with hashedpassword
     if (user && (await bcrypt.compare(password, user.password))) {
-      const accessToken = jwt.sign(
-        {
-          user: {
-            username: user.username,
-            email: user.email,
-            id: user.id,
-          },
-        },
-        process.env.ACCESS_TOKEN_SECERT,
-        { expiresIn: "15m" }
-      );
-      res.status(200).json({ accessToken });
+        const accessToken = jwt.sign(
+            {
+                user: {
+                    username: user.username,
+                    email: user.email,
+                    id: user.id,
+                },
+            },
+            process.env.ACCESS_TOKEN_SECERT,
+            { expiresIn: "15m" }
+        );
+        res.status(200).json({ accessToken });
     } else {
-      res.status(401);
-      throw new Error("email or password is not valid");
+        res.status(401);
+        throw new Error("email or password is not valid");
     }
 });
 
@@ -75,7 +75,7 @@ const loginUser = asyncHandler(async (req, res) => {
 //@route GET /api/users/current
 //@access private
 const currentUser = asyncHandler(async (req, res) => {
-    res.json({ message: "Current User Information" });
+    res.json(req.user);
 });
 
 module.exports = { registerUser, loginUser, currentUser };
