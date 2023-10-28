880fc2b8f7caa6bb5950b5bc1ba6504621cad4f1
Marwan <marwaneg@aucegypt.edu>
2023-10-29 01:13:34 +0200

    Finalize contact with user functionalities

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -4,7 +4,7 @@ const Contact = require("../models/contactModel");
 //@route GET /api/contacts
 //@access private
 const getContacts = asyncHandler(async (req, res) => {
-  const contacts = await Contact.find();
+  const contacts = await Contact.find({ user_id: req.user.id });
   res.status(200).json(contacts);
 });
 
@@ -22,7 +22,7 @@ const createContact = asyncHandler(async (req, res) => {
     name,
     email,
     phone,
-    // user_id: req.user.id,
+    user_id: req.user.id,
   });
 
   res.status(201).json(contact);
@@ -50,10 +50,10 @@ const updateContact = asyncHandler(async (req, res) => {
     throw new Error("Contact not found");
   }
 
-//   if (contact.user_id.toString() !== req.user.id) {
-//     res.status(403);
-//     throw new Error("User don't have permission to update other user contacts");
-//   }
+  if (contact.user_id.toString() !== req.user.id) {
+    res.status(403);
+    throw new Error("User don't have permission to update other user contacts");
+  }
 
   const updatedContact = await Contact.findByIdAndUpdate(
     req.params.id,
@@ -73,11 +73,11 @@ const deleteContact = asyncHandler(async (req, res) => {
     res.status(404);
     throw new Error("Contact not found");
   }
-//   if (contact.user_id.toString() !== req.user.id) {
-//     res.status(403);
-//     throw new Error("User don't have permission to update other user contacts");
-//   }
-  await Contact.deleteOne();
+  if (contact.user_id.toString() !== req.user.id) {
+    res.status(403);
+    throw new Error("User don't have permission to update other user contacts");
+  }
+  await Contact.deleteOne({ _id: req.params.id });
   res.status(200).json(contact);
 });
 
--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -7,17 +7,10 @@ const {
   updateContact,
   deleteContact,
 } = require("../controllers/contactController");
-// const validateToken = require("../middleware/validateTokenHandler");
+const validateToken = require("../middleware/validateTokenHandler");
 
-// router.use(validateToken);
+router.use(validateToken);
 router.route("/").get(getContacts).post(createContact);
 router.route("/:id").get(getContact).put(updateContact).delete(deleteContact);
-
-module.exports = router;
-
-
-router.route("/").get((req, res) => {
-    res.status(200).json({message: "Get all messages"})
-});
 
 module.exports = router;
